4092af60f83b8435eb543dfb37b215db2711aa8e
Agus9217 <justinibarrola92@gmail.com>
2024-01-03 23:43:00 -0300

    terminando seed

--- src/config/data/seed/seed.ts
+++ src/config/data/seed/seed.ts
@@ -2,6 +2,7 @@ import { envs } from "../../envs";
 import { CategoryModel, ProductModel, UserModel } from "../mongo/models";
 
 import { MongoDatabase } from "../mongo/mongo-database";
+import { seedData } from "./data";
 
 (async () => {
   MongoDatabase.connectDb({
@@ -12,12 +13,37 @@ import { MongoDatabase } from "../mongo/mongo-database";
   await MongoDatabase.disconnectDb();
 })();
 
+const randomNumber = (x: number) => {
+  return Math.floor(Math.random() * x);
+};
+
 async function main() {
   await Promise.all([
     UserModel.deleteMany(),
     CategoryModel.deleteMany(),
     ProductModel.deleteMany(),
   ]);
+
+  const users = await UserModel.insertMany(seedData.users);
+
+  const categories = await CategoryModel.insertMany(
+    seedData.categories.map((category) => {
+      return {
+        ...category,
+        user: users[0]._id,
+      };
+    })
+  );
+
+  const products = await ProductModel.insertMany(
+    seedData.products.map((product) => {
+      return {
+        ...product,
+        user: users[randomNumber(seedData.users.length) - 1]._id,
+        category: categories[randomNumber(seedData.categories.length) - 1]._id,
+      };
+    })
+  );
 
   console.log("Seed created");
 }
